539c5ccdca2cb05420103645c77ac87e687d0fb3
darthanne <62066644+darthanne@users.noreply.github.com>
2020-05-30 17:30:43 +0200

    Add files via upload

--- Fagprojekt.py
+++ Fagprojekt.py
@@ -1,7 +1,24 @@
-from IM import*
-from FindShore_function import isShore
+import scipy.io
+import scipy.sparse
+import numpy as np
+import pandas as pd
+import numpy.ma as ma
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from matplotlib import animation
+import matplotlib.cm as cm
+from IPython import display
+import matplotlib.colors as colors
+import matplotlib.patches as patches
 
+from FindShore import isShore
+
+
+
+#
 # Inputs
+#
+
 # P-Matrices
 P1 = scipy.io.loadmat('P1.mat')['P1'].tocsr()
 P2 = scipy.io.loadmat('P2.mat')['ans'].tocsr()
@@ -11,28 +28,11 @@ P5 = scipy.io.loadmat('P5.mat')['ans'].tocsr()
 P6 = scipy.io.loadmat('P6.mat')['ans'].tocsr()
 P = [P1, P2, P3, P4, P5, P6]
 
-# Influx transport matrix
-F1 = scipy.io.loadmat('F.mat')['F1'].tocsr()
-F2 = scipy.io.loadmat('F.mat')['F2'].tocsr()
-F3 = scipy.io.loadmat('F.mat')['F3'].tocsr()
-F4 = scipy.io.loadmat('F.mat')['F4'].tocsr()
-F5 = scipy.io.loadmat('F.mat')['F5'].tocsr()
-F6 = scipy.io.loadmat('F.mat')['F6'].tocsr()
-F = [F1, F2, F3, F4, F5, F6]
-
-# Outflux transport matrix
-E1 = scipy.io.loadmat('E.mat')['E1'].tocsr()
-E2 = scipy.io.loadmat('E.mat')['E2'].tocsr()
-E3 = scipy.io.loadmat('E.mat')['E3'].tocsr()
-E4 = scipy.io.loadmat('E.mat')['E4'].tocsr()
-E5 = scipy.io.loadmat('E.mat')['E5'].tocsr()
-E6 = scipy.io.loadmat('E.mat')['E6'].tocsr()
-E = [E1, E2, E3, E4, E5, E6]
-
 # Land
 landpoints = np.array(scipy.io.loadmat('landpoints.mat')['landpoints'])
 
-"""
+# Create shoreline file
+""" 
 # Find shoreline as any land with adjacent water
 shoreline = np.zeros(65341, dtype=bool)
 shoreline = shoreline.reshape((181, 361))
@@ -41,6 +41,7 @@ for y in range(181):
     for x in range(361):
         shoreline[y, x] = isShore(landpoints, y, x)
 """
+
 # Import from file
 shoreline = np.array(pd.read_csv("shoreline.txt", header=None))
 
@@ -66,8 +67,6 @@ landpoints[:10, :] = 1
 landpoints[:25, 95:270] = 1
 
 s = np.flipud(ma.masked_where(landpoints == 1, s))
-plt.matshow(s)
-plt.show()
 
 s = scipy.sparse.csr_matrix(s.reshape(65341))
 
@@ -76,25 +75,6 @@ s = scipy.sparse.csr_matrix(s.reshape(65341))
 def matrixStep(i, c):
     return c @ P[i % 6]
 
-
-a = matrixStep(0, s)
-b = matrixStep(1, a)
-
-plt.matshow(a.todense().reshape((181, 361)))
-plt.show()
-
-DeltaP = np.sum(b.todense()[156:, :] - a.todense()[156:, :])
-DeltaIO = scipy.sum(a @ F2 - a @ E2)
-
-print(DeltaP)
-print(DeltaIO)
-
-fig = plt.figure()
-
-ax = plt.axes(xlim=(0, 361), ylim=(0, 181))
-line = ax.matshow(np.roll(matrixStep(0, s).todense().reshape([181, 361]), axis=1, shift=180))
-sl = ax.matshow(ma.masked_where(shoreline == 1, shoreline), alpha=1, cmap=ListedColormap(['k', 'w']))
-plt.show()
 
 cmap = plt.cm.get_cmap('RdBu_r', 28)
 
